5f06a4f661a384fe1852fef074c102d228b66641
Sylv <plop@lol.com>
2021-11-08 19:02:27 +0100

    init

---
+++ config.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+import os
+
+""" Bot Configuration """
+
+
+class DefaultConfig:
+    """ Bot Configuration """
+
+    PORT = 3978
+    APP_ID = os.environ.get("MicrosoftAppId", "B8ls6I3mBMEXKcluov")
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "ac741b4a-08c6-45e4-b2fa-40dd46e85ed9")
+    LUIS_APP_ID = os.environ.get("LuisAppId", "")
+    LUIS_API_KEY = os.environ.get("LuisAPIKey", "")
+    # LUIS endpoint host name, ie "westus.api.cognitive.microsoft.com"
+    LUIS_API_HOST_NAME = os.environ.get("LuisAPIHostName", "")
